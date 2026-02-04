26098f7cbc492fc792946f679db5b92e965ef789
Nam Vo <vhnam2504@gmail.com>
2026-02-05 03:14:52 +0700

    refactor: update type

---
+++ src/models/certificate.ts
@@ -0,0 +1,8 @@
+export interface Certificate {
+    title: string;
+    description: string;
+    link: string;
+    completed: boolean;
+    certificateUrl: string | null;
+    completedDate: string | null;
+}
