d0d521f03c4eb4a2986d81e548837a784f40f5de
Nam Vo <nam.vh@wearecare.sg>
2026-03-06 17:32:51 +0700

    chore: add Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  adapter: netlify(),
   output: "static",
   compressHTML: true,
   build: {
